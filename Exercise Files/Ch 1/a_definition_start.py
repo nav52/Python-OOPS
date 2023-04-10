ba90ad2635d74018784224c958e613982f6f3c8e
Naveen Karanth <naveenfaster@gmail.com>
2023-04-10 13:02:09 +0530

    Chapter 1 complete

---
+++ Exercise Files/Ch 1/a_definition_start.py
@@ -0,0 +1,16 @@
+# Python Object Oriented Programming by Joe Marini course example
+# Basic class definitions
+
+
+# TODO: create a basic class
+class Book():
+    def __init__(self, title):
+        self.title = title
+
+# TODO: create instances of the class
+b1 = Book("War and Peace")
+b2 = Book("The Catcher in the Rye")
+
+# TODO: print the class and property
+print(b1)
+print(b1.title)
---
+++ Exercise Files/Ch 1/b_instance_start.py
@@ -0,0 +1,40 @@
+# Python Object Oriented Programming by Joe Marini course example
+# Using instance methods and attributes
+
+
+class Book:
+    # the "init" function is called when the instance is
+    # created and ready to be initialized
+    def __init__(self, title, author, pages, price):
+        self.title = title
+        # TODO: add properties
+        self.author = author
+        self.pages = pages
+        self.price = price
+        self.__secret = "this is a secret attribute"
+    # TODO: create instance methods
+    def getprice(self):
+        if hasattr(self, "_discount"):
+            return self.price - (self.price * self._discount)
+        else:
+            return self.price
+    
+    def setdiscount(self, amount):
+        self._discount = amount
+
+# TODO: create some book instances
+b1 = Book("War and Peace", "Leo Tolstoy", 1225, 39.95)
+b2 = Book("The Catcher in the Rye", "JD Salinger", 234, 29.95)
+
+# TODO: print the price of book1
+print(b1.getprice())
+
+# TODO: try setting the discount
+print(b2.getprice())
+b2.setdiscount(0.25)
+print(b2.getprice())
+
+
+# TODO: properties with double underscores are hidden by the interpreter
+## print(b1.__secret) will throw an error stating __secret isn't found. It can be overridden/accessed by adding the _className prefix
+print(b1._Book__secret)
