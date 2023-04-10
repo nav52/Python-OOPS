cdc861b3a4114f1c1d38beebb7f9671b34ab2c21
Naveen Karanth <naveenfaster@gmail.com>
2023-04-10 15:23:40 +0530

    Chapter 2 Completed !

---
+++ Exercise Files/Ch 2/c_multiple_start.py
@@ -0,0 +1,33 @@
+# Understanding multiple inheritance
+
+
+class A:
+    def __init__(self):
+        super().__init__()
+        self.foo = "foo"
+        self.name = "Class A"
+
+class B:
+    def __init__(self):
+        super().__init__()
+        self.bar = "bar"
+        self.name = "Class B"
+
+class C(A, B):
+    def __init__(self):
+        super().__init__()
+
+    def showprops(self):
+        print(self.foo)
+        print(self.bar)
+        print(self.name)
+
+
+c = C()
+c.showprops()
+
+## Uses Method Resolution Order..
+# C   --> A
+#     --> B
+
+print(C.__mro__)  ## (<class '__main__.C'>, <class '__main__.A'>, <class '__main__.B'>, <class 'object'>)
